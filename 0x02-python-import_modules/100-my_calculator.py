fb61e406c55a8c45b20bdc374826a799190c9029
JamalGuyo <jamalguyo08@gmail.com>
2022-09-23 00:43:25 +0300

    100 my calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv, exit
-if __name__ == '__main__':
-    count = len(argv) - 1
-    if count == 3:
-        if argv[2] not in ['+', '-', '*', '/']:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
-        else:
-            if argv[2] == '+':
-                print("{} {} {} = {}"
-                      .format(argv[1], argv[2], argv[3],
-                              add(int(argv[1]), int(argv[3]))))
-            if argv[2] == '-':
-                print("{} {} {} = {}"
-                      .format(argv[1], argv[2], argv[3],
-                              sub(int(argv[1]), int(argv[3]))))
-            if argv[2] == '*':
-                print("{} {} {} = {}"
-                      .format(argv[1], argv[2], argv[3],
-                              mul(int(argv[1]), int(argv[3]))))
-            if argv[2] == '/':
-                print("{} {} {} = {}"
-                      .format(argv[1], argv[2], argv[3],
-                              div(int(argv[1]), int(argv[3]))))
-    else:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        print(f'Count is ::::: {count}')
-        exit(1)
+
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
