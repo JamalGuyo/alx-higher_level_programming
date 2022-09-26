5d0a145ea409eb300fc47d9a70874882259e0942
JamalGuyo <jamalguyo08@gmail.com>
2022-09-26 21:28:03 +0300

    103 fast alphabet

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    s = []
     new_str = ""
     for i in my_string:
         if i == 'c' or i == 'C':
             continue
-        s.append(i)
-    return new_str.join(s)
+        new_str+=i
+    return new_str
