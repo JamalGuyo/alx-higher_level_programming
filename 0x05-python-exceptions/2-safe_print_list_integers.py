16f6c397bd11df20ab92ae8ce9e5122044efdb64
JamalGuyo <jamalguyo08@gmail.com>
2022-10-11 20:41:47 +0300

    0 safe print list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    for i in range(x):
+        if type(my_list) == int:
+            print("{:d}".format(my_list[i]), end="")
+        else:
+            continue
